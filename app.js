29cb99e1be5721250bf4edc55f62e26db1da849d
zorgonide <ammar1998@gmail.com>
2023-06-27 14:32:42 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+http.createServer(function (req, res) {
+ res.write("On the way to being a full stack engineer!");
+ res.end();
+}).listen(3000);
+
+console.log("Server started on port 3000");
+
