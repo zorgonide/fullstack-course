9075965067e65aad5e833ecab0e40464921751f5
zorgonide <ammar1998@gmail.com>
2023-06-27 22:08:02 +0100

    reverting back to old file

--- index-ws.js
+++ index-ws.js
@@ -1,53 +1,33 @@
 const express = require("express");
 const server = require("http").createServer();
 const app = express();
-const PORT = 3000;
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
 });
 
 server.on("request", app);
-
-server.listen(PORT, function () {
-  console.log("Listening on " + PORT);
+server.listen(3000, () => {
+  console.log("Express server started at port 3000");
 });
 
-/** Websocket **/
+// begin websocket server
 const WebSocketServer = require("ws").Server;
-
 const wss = new WebSocketServer({ server: server });
 
-wss.on("connection", function connection(ws) {
-  const numClients = wss.clients.size;
-
-  console.log("clients connected: ", numClients);
-
-  wss.broadcast(`Current visitors: ${numClients}`);
-
-  if (ws.readyState === ws.OPEN) {
-    ws.send("welcome!");
-  }
-
-  ws.on("close", function close() {
-    wss.broadcast(`Current visitors: ${wss.clients.size}`);
-    console.log("A client has disconnected");
-  });
-
-  ws.on("error", function error() {
-    //
+wss.on("connection", (ws) => {
+  //check
+  let numClients = wss.clients.size;
+  console.log("Clients connected: " + numClients);
+  wss.broadcast("Current visitors: " + numClients);
+  ws.readyState === ws.OPEN && ws.send("Welcome to my server - Ammar");
+  ws.on("close", () => {
+    wss.broadcast("A client has left the server");
+    console.log("Client disconnected");
   });
 });
-
-/**
- * Broadcast data to all connected clients
- * @param  {Object} data
- * @void
- */
-wss.broadcast = function broadcast(data) {
-  console.log("Broadcasting: ", data);
-  wss.clients.forEach(function each(client) {
+wss.broadcast = function (data) {
+  wss.clients.forEach(function (client) {
     client.send(data);
   });
 };
-/** End Websocket **/
