932d01789a07a979ef0a642b81b81367981ff1f9
zorgonide <ammar1998@gmail.com>
2023-06-27 20:26:42 +0100

    test edit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const http = require("http");
 
-http.createServer(function (req, res) {
- res.write("On the way to being a full stack engineer!");
- res.end();
-}).listen(3000);
+http
+  .createServer(function (req, res) {
+    res.write("On the way to being an engineer!");
+    res.end();
+  })
+  .listen(3000);
 
 console.log("Server started on port 3000");
-
